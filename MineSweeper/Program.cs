0c14d2328729c63e74f78cdcb8bd870edf040ea0
penguinkim <cutlake2000@kakao.com>
2024-07-01 16:25:43 +0900

    [NEW] 게임 재시작 기능 수정 & 초기값 입력 예외처리

--- MineSweeper/Program.cs
+++ MineSweeper/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MineSweeper.MineSweeper;
+﻿using System.Data;
+
+namespace MineSweeper.MineSweeper;
 
 /// <summary>
 /// Program 클래스 : 초기 설정값 (보드판 너비, 높이, 지뢰 수)을 입력 받고 게임을 시작합니다.
@@ -19,7 +21,7 @@ public static class Program
 
             gameManager.Start(); // 게임 시작
 
-        } while (NewGame());
+        } while (RestartGame());
     }
 
     /// <summary>
@@ -32,19 +34,65 @@ public static class Program
     {
         Console.Clear();
         Console.WriteLine("지뢰찾기 v1.0.0");
-        Console.Write("# 맵의 X축 값을 입력하세요 : ");
-        mapX = int.Parse(Console.ReadLine() ?? string.Empty);
-        Console.Write("# 맵의 Y축 값을 입력하세요 : ");
-        mapY = int.Parse(Console.ReadLine() ?? string.Empty);
-        Console.Write("# 전체 지뢰 수를 입력하세요 : ");
-        entireMineCount = int.Parse(Console.ReadLine() ?? string.Empty);
+        mapX = ReadIntFromConsole("# 맵의 X축 값을 입력하세요 : ");
+        mapY = ReadIntFromConsole("# 맵의 Y축 값을 입력하세요 : ");
+        entireMineCount = ReadIntFromConsole("# 전체 지뢰 수를 입력하세요 : ", 1, mapX * mapX);
+    }
+
+    /// <summary>
+    /// 사용자로부터 정수를 입력 받습니다. 정수로 파싱이 불가능하거나, 0보다 작다면 다시 입력 받습니다.
+    /// </summary>
+    /// <param name="message">출력 메시지</param>
+    /// <returns>입력 받은 정수 값</returns>
+    private static int ReadIntFromConsole(string message)
+    {
+        int output;
+        bool isValid;
+
+        do
+        {
+            Console.Write(message);
+            
+            var input = Console.ReadLine();
+            isValid = int.TryParse(input, out output) && output > 0;
+
+            if (isValid) continue;
+            Console.WriteLine("!! 올바른 정수 값을 입력해주세요 !!");
+        } while (!isValid); // 정수로 파싱이 가능하고, 0보다 큰 경우에만 루프를 종료합니다.
+
+        return output;
+    }
+
+    /// <summary>
+    /// 사용자로부터 정수를 입력 받습니다. 정수로 파싱이 불가능하거나, 범위 밖의 정수라면 다시 입력 받습니다.
+    /// </summary>
+    /// <param name="message">출력 메시지</param>
+    /// <param name="min">최소</param>
+    /// <param name="max">최대</param>
+    /// <returns>입력 받은 정수 값</returns>
+    private static int ReadIntFromConsole(string message, int min, int max)
+    {
+        int output;
+        bool isValid;
+
+        do
+        {
+            Console.Write(message);
+            var input = Console.ReadLine();
+            isValid = int.TryParse(input, out output) && min < output && output < max;
+            
+            if (isValid) continue;
+            Console.WriteLine($"올바른 범위 내 정수를 입력해주세요. (범위: {min} - {max})");
+        } while (!isValid);  // 정수로 파싱이 가능하고, min 이상 max 이하의 경우에만 루프를 종료합니다.
+
+        return output;
     }
     
     /// <summary>
     /// 사용자로부터 새 게임을 진행할지 여부를 입력받습니다.
     /// </summary>
     /// <returns>새 게임 진행 여부</returns>
-    private static bool NewGame()
+    private static bool RestartGame()
     {
         Console.WriteLine("# 새 게임을 진행하시겠습니까? Yes / No");
         Console.Write("> ");
